e6ffc0bc0a7d5955f31b20017f5a8d061908c792
Matias Jensen <sejemus@yahoo.dk>
2017-09-20 11:34:54 +0200

    Fixed header and small text

--- information.php
+++ information.php
@@ -11,6 +11,14 @@
 			color:#FFFFFF;
 		}
         
+        .header {
+            font-size:30px;
+        }
+        
+        .smalltext {
+            font-size:12px;
+        }
+        
         .postlight {
 			background-color:#969696;
 			text-align:left;
@@ -27,8 +35,8 @@
 		</style>
 	</head>
 <div class="postlight">
-    <h1>[Post Title]</h1>
-    <small>Posted on [datestamp] by [Author]</small>
+    <h1 class="header">[Post Title]</h1>
+    <small class="smalltext">Posted on [datestamp] by [Author]</small>
     <p>Lorem ipsum dolor sit amet, nonumes voluptatum mel ea, cu case ceteros cum. Novum commodo malorum vix ut. Dolores consequuntur in ius, sale electram dissentiunt quo te. Cu duo omnes invidunt, eos eu mucius fabellas. Stet facilis ius te, quando voluptatibus eos in. Ad vix mundi alterum, integre urbanitas intellegam vix in.</p>
     <p>Eum facete intellegat ei, ut mazim melius usu. Has elit simul primis ne, regione minimum id cum. Sea deleniti dissentiet ea. Illud mollis moderatius ut per, at qui ubique populo. Eum ad cibo legimus, vim ei quidam fastidii.</p>
     <p>Quo debet vivendo ex. Qui ut admodum senserit partiendo. Id adipiscing disputando eam, sea id magna pertinax concludaturque. Ex ignota epicurei quo, his ex doctus delenit fabellas, erat timeam cotidieque sit in. Vel eu soleat voluptatibus, cum cu exerci mediocritatem. Malis legere at per, has brute putant animal et, in consul utamur usu.</p>
@@ -36,8 +44,8 @@
     <p>Ea duo atqui incorrupte, sed rebum regione suscipit ex, mea ex dicant percipit referrentur. Dicat luptatum constituam vix ut. His vide platonem omittantur id, vel quis vocent an. Ad pro inani zril omnesque. Mollis forensibus sea an, vim habeo adipisci contentiones ad, tale autem graecis ne sit.</p>
 </div>
 <div class="postdark">
-    <h1>[Post Title]</h1>
-    <small>Posted on [datestamp] by [Author]</small>
+    <h1 class="header">[Post Title]</h1>
+    <small class="smalltext">Posted on [datestamp] by [Author]</small>
     <p>[Content]</p>
 </div>
 </html>
