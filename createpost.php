8ade8951a1647b59fb219e9af986b2961b8d0b27
Matias Jensen <sejemus@yahoo.dk>
2017-09-20 12:53:17 +0200

    Quick and dirty template for creating posts

---
+++ createpost.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<style type="text/css">
+		body{ background-color:#37393D; }
+        
+		* {
+			text-align:left;
+			font-family:"Classic Robot";
+            font-size: 20px;
+		}
+        
+        .whitetext {
+            font-size: 25px;
+            color:#FFFFFF;
+        }
+		</style>
+	</head>
+    
+    <form action="" method="post">
+        <input class="button" type="submit" value="Publish Post"><br/>
+        <p class="whitetext">Title:</p>
+        <input type="text" name="title"><br/>
+        <p class="whitetext">Content:</p>
+        <textarea rows="20" cols="100" name="content"></textarea><br/>
+    </form>
+</html>
